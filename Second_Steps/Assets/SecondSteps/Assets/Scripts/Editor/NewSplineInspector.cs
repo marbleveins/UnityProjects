a833afcea0927c75cf175d0e2898afe0f0ce2563
Agus <agustinbulgach@gmail.com>
2018-02-08 00:35:15 -0300

    Nuevo Spline de 0

---
+++ Second_Steps/Assets/SecondSteps/Assets/Scripts/Editor/NewSplineInspector.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+[CustomEditor(typeof(NewSpline))]
+public class NewSplineInspector : Editor {
+
+    private Quaternion handleRotation;
+    private Transform handleTransform;
+    private const float handleSize = 0.04f;
+    private const float pickSize = 0.06f;
+
+    private bool splineOn = false;
+
+    private NewSpline spline;
+
+    private int selectedNodeIndex;
+    private Node selectedNode;
+
+    private void OnSceneGUI()
+    {
+        spline = target as NewSpline;
+
+        if (splineOn)
+        {
+            handleTransform = spline.transform;
+            handleRotation = Tools.pivotRotation == PivotRotation.Local ? handleTransform.rotation : Quaternion.identity;
+
+            DrawHandles();
+            DrawCurves();
+        }
+        else
+        {
+            StartNewSpline();
+        }
+
+    }
+
+
+    private void StartNewSpline()
+    {
+        spline.AddNewDefaultCurve();
+
+
+
+
+
+        splineOn = true;
+    }
+
+
+    private void DrawHandles()
+    {
+        foreach(Node node in spline.nodes)
+        {
+            Handles.Button(node.location, handleRotation, 2f * handleSize, 2f * pickSize, Handles.DotHandleCap);
+        }
+    }
+
+    private void DrawCurves()
+    {
+        for (int i = 0; i < spline.curves.Count; i++)
+        {
+            Handles.DrawBezier(spline.transform.TransformPoint(spline.curves[i].n1.location),
+                spline.transform.TransformPoint(spline.curves[i].n2.location),
+                spline.transform.TransformPoint(spline.curves[i].n1.direction),
+                spline.transform.TransformPoint(spline.curves[i].GetInverseDirection()),
+                Color.white,
+                null,
+                2f);
+            Handles.DrawLine(spline.curves[i].n1.location, spline.curves[i].c1.location);
+            Handles.DrawLine(spline.curves[i].n2.location, spline.curves[i].c2.location);
+        }
+    }
+
+}
---
+++ Second_Steps/Assets/SecondSteps/Assets/Scripts/Spline/NewSpline.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewSpline : MonoBehaviour
+{
+
+    public List<Node> nodes = new List<Node>();
+    public List<Curve> curves = new List<Curve>();
+
+
+
+    public void AddNewDefaultCurve()
+    {
+        Node n1, n2, c1, c2;
+        if (nodes.Count == 0)
+        {
+            n1 = new Node(new Vector3(0, 0, 0), new Vector3(0, 0, 0));
+        }
+        else
+        {
+            n1 = nodes[nodes.Count - 1];
+        }
+        n2 = new Node(new Vector3(n1.location.x + 10, n1.location.y, n1.location.z), new Vector3(n1.direction.x + 10, n1.direction.y, n1.direction.z));
+
+        c1 = new Node(new Vector3(n1.location.x, n1.location.y + 10, n1.location.z), new Vector3(n1.direction.x, n1.direction.y + 10, n1.direction.z));
+        c2 = new Node(new Vector3(n2.location.x, n2.location.y + 10, n2.location.z), new Vector3(n2.direction.x, n2.direction.y + 10, n2.direction.z));
+
+        nodes.Add(n1);
+        nodes.Add(c1);
+        nodes.Add(c2);
+        nodes.Add(n2);
+
+        Curve curve = new Curve(n1, n2);
+        curve.c1 = c1;
+        curve.c2 = c2;
+        curves.Add(curve);
+
+    }
+}
