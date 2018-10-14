543c7fd250d1861d9ab70802883fe651e10aa9cb
Agus <agustinbulgach@gmail.com>
2018-10-14 17:08:53 -0300

    default shape centered

--- Second_Steps/Assets/SecondSteps/Assets/Scripts/Common/CommonHelperForNow.cs
+++ Second_Steps/Assets/SecondSteps/Assets/Scripts/Common/CommonHelperForNow.cs
@@ -10,15 +10,15 @@ public class CommonHelperForNow
     {
         List<Vertex> vertices = new List<Vertex>
         {
-            new Vertex(new Vector2(0, 0f), new Vector2(0, -1), 0),
+            new Vertex(new Vector2(0, -1f), new Vector2(0, -1), 0),
 
-            new Vertex(new Vector2(-1, 0.5f), new Vector2(-1, -1), 0.66f),
-            new Vertex(new Vector2(-1.5f, 1.5f), new Vector2(-1, 0), 0.33f),
-            new Vertex(new Vector2(-1, 2.5f), new Vector2(-1, 1.5f), 0.33f),
+            new Vertex(new Vector2(-1, -0.5f), new Vector2(-1, -1), 0.66f),
+            new Vertex(new Vector2(-1.5f, 0.5f), new Vector2(-1, 0), 0.33f),
+            new Vertex(new Vector2(-1, 1.5f), new Vector2(-1, 1.5f), 0.33f),
 
-            new Vertex(new Vector2(1, 2.5f), new Vector2(1, 1.5f), 0),
-            new Vertex(new Vector2(1.5f, 1.5f), new Vector2(1, 0), 0.66f),
-            new Vertex(new Vector2(1, 0.5f), new Vector2(1, -1), 0.33f)
+            new Vertex(new Vector2(1, 1.5f), new Vector2(1, 1.5f), 0),
+            new Vertex(new Vector2(1.5f, 0.5f), new Vector2(1, 0), 0.66f),
+            new Vertex(new Vector2(1, -0.5f), new Vector2(1, -1), 0.33f)
         };
 
         return vertices;
